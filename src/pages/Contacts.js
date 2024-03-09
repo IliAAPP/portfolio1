28a9b352dfc54df074007440921e8c0ec0f04193
IliAAPP <ilya.appolonov@mail.ru>
2024-03-09 21:02:09 +0300

    myy_portfolio
    myy_portfolio

---
+++ src/pages/Contacts.js
@@ -0,0 +1,32 @@
+const Contacts = () => {
+    return (
+		<main className="section">
+			<div className="container">
+				<h1 className="title-1">Contacts</h1>
+
+				<ul className="content-list">
+					<li className="content-list__item">
+						<h2 className="title-2">Location</h2>
+						<p>Krasnodar, Russia</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Telegram / WhatsApp</h2>
+						<p>
+							<a href="tel:+79951866619">+7 (995) 186-66-19</a>
+						</p>
+					</li>
+					<li className="content-list__item">
+						<h2 className="title-2">Email</h2>
+						<p>
+							<a href="mailto:IliAAPP@mail.ru">
+								IliAAPP@mail.ru
+							</a>
+						</p>
+					</li>
+				</ul>
+			</div>
+		</main>
+	);
+}
+
+export default Contacts;
